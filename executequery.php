5888cf7e609da5a6f5a24f00f51ae03e93a6b26f
ar664 <ar664@njit.edu>
2019-05-06 13:01:20 -0400

    Fixed results not going

--- executequery.php
+++ executequery.php
@@ -2,6 +2,11 @@
 
 include 'connect.php';
 
+if(!isset($_POST["SQL"]))
+{
+    die('{ "POST":"SQL not set" }');
+}
+
 $sql = $_POST["SQL"];
 $result = mysqli_query($conn, $sql);
 $error = mysqli_error($conn);
@@ -9,7 +14,21 @@ if($error)
 {
     echo '{ "dbSuccess":false, "SQL_ERROR": ' . json_encode($error) . '}';
 } else {
-    echo json_encode($result);
+    $arr = array();
+    $i = 0;
+    if(mysqli_num_rows($result) > 0)
+    {
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $arr[$i] = $row;
+            $i = $i + 1;
+        }
+        echo json_encode($arr);
+    } else 
+    {
+        echo '{ "dbSuccess": true }';
+    }
+    //echo json_encode($result);
 }
 
 mysqli_close($conn);
